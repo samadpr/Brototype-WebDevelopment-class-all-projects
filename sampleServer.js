e623554d945c9c1a280c3b4f28bd7781891e19c2
Abdul Samad <codinguse341@gmal.com>
2022-07-05 02:22:21 +0530

    web development 7th class are completed

--- sampleServer.js
+++ sampleServer.js
@@ -17,13 +17,38 @@ function server(req,res){
 
 var http=require('http')
 var fs=require('fs')
+var url=require('url')
 
 http.createServer(function(req,res){
+
+    var q=url.parse(req.url,true)
+    //console.log(q.pathname)
+
+    if(q.pathname==='/'){
+
     
-    fs.readFile('samplehtml.html',function (err,data){
+    fs.readFile('samplehome.html',function (err,data){
         res.writeHead(200,{'Content-type':'text/html'})
         res.write(data)
         res.end()    
     })
+}else if(q.pathname==='/signup'){
+    fs.readFile('samplehtml.html',(err,data) => {
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write(data)
+        res.end()
+    })
     
-}).listen(5000)
+}else if(q.pathname==='/signupaction'){
+
+    console.log(q.query.firstname)
+    res.writeHead(200,{'Content-type':'text/html'})
+    res.write('<h1>'+'Hi '+q.query.firstname+'</h1>')
+    res.end()
+}else{
+    res.writeHead(404,{'Content-Type':'text/html'})
+    res.write('error')
+    res.end()
+}
+
+}).listen(5000,() => console.log("Server Started"))
