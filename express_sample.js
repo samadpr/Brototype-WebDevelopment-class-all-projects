1cc589684426de425bea07d76637a696e5ef78c3
Abdul Samad <codinguse341@gmal.com>
2022-07-05 13:50:34 +0530

    web devlopment 8th class are completed

---
+++ express_sample.js
@@ -0,0 +1,41 @@
+const express=require('express')
+const path=require('path')
+
+const app=express()
+/*
+app.use(function(req,res,next){
+    console.log('start')
+
+
+    next()
+})
+*/
+//app.use((req,res,next) => console.log('hello') ,next())
+
+app.get('/signup',function(req,res,next){
+    res.sendFile(path.join(__dirname,'samplehtml.html'))
+    console.log('middle')
+    next()
+})
+
+//app.get('/signup',(req,res) => res.sendFile(path.join(__dirname,'samplehtml.html')))
+/*
+app.use(function(req,res){
+    console.log('end')
+})
+*/
+//app.use((req,res) => console.log('end'))
+
+app.post('/signup',(req,res) => res.send('account created'))
+
+app.get('/about',(req,res) => res.send('about'))
+
+
+app.listen(5000,function(){
+
+    console.log(__filename)
+    console.log('Server Started')
+})
+
+
+//app.listen(3000,() => console.log(__dirname).log('Server Started'))
